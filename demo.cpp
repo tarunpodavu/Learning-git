4fddc3383da553724f120de8e287e5e350c07620
unknown <tarunkumarpodavu4@gmail.com>
2020-05-21 11:14:29 +0530

    Demo file commited

---
+++ demo.cpp
@@ -0,0 +1,10 @@
+//Demo CPP
+#include<iostream>
+using namespace std;
+int main(){
+    std::cout<<"Hello!!!!!!!!!!!!!!!!!1\n";
+    char ar[] {'C','+','+'};
+    for(int i=0;i<3;i++)
+    cout<<ar[i]<<"\t";
+    return 0;
+}
